a7602238b761206fe9aba32e8870002ce5164daf
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-01-21 19:31:20 +0000

    Rebase from upstream:
    add groups sheet 1
    
    harder group questions
    
    add bonus puzzle
    
    add solutions to sheet 4
    
    fix import
    
    solutions to sheet 5
    
    clarify linarith. Thanks John Moore.
    
    more reals stuff
    
    add sheet 6
    
    add subgroups
    
    group homs
    
    add docstring
    
    solutions to sheets 1-3
    
    add sheet 4 solutions

--- src/solutions/section02reals/sheet5.lean
+++ src/solutions/section02reals/sheet5.lean
@@ -12,10 +12,13 @@ import solutions.section02reals.sheet3 -- import the definition of `tendsto` fro
 theorem tendsto_neg {a : ℕ → ℝ} {t : ℝ} (ha : tendsto a t) :
   tendsto (λ n, - a n) (-t) :=
 begin
-  rw tendsto_def at ha ⊢,
-  simp_rw abs_sub_comm,
-  norm_num,
-  exact ha
+  rw tendsto_def at *,
+  have h : ∀ n, |a n - t| = | -a n - -t|,
+  { intro n,
+    rw abs_sub_comm,
+    congr' 1,
+    ring },
+  simpa [h] using ha,
 end
 
 /-
@@ -35,20 +38,25 @@ theorem tendsto_add {a b : ℕ → ℝ} {t u : ℝ}
   (ha : tendsto a t) (hb : tendsto b u) :
   tendsto (λ n, a n + b n) (t + u) :=
 begin
-  rw tendsto_def at ha hb ⊢,
+  rw tendsto_def at *,
+  -- let ε > 0 be arbitrary
   intros ε hε,
+  --  There's a bound X such that if n≥X then a(n) is within ε/2 of t
   specialize ha (ε/2) (by linarith),
-  specialize hb (ε/2) (by linarith),
-  cases ha with Ba ha,
-  cases hb with Bb hb,
-  use (Ba + Bb),
-  intros n hab,
-  specialize ha n (by linarith),
-  specialize hb n (by linarith),
-  have h : |a n - t| + |b n - u| < ε := by linarith,
-  have h : |(a n - t) + (b n - u)| ≤ |a n - t| + |b n - u| := by apply abs_add,
-  ring_nf at h ⊢,
-  linarith
+  cases ha with X hX,
+  --  There's a bound Y such that if n≥Y then b(n) is within ε/2 of u
+  obtain ⟨Y, hY⟩ := hb (ε/2) (by linarith),
+  --  use max(X,Y),
+  use max X Y,
+  -- now say n ≥ max(X,Y)
+  intros n hn,
+  rw max_le_iff at hn,
+  specialize hX n hn.1,
+  specialize hY n hn.2,
+  --  Then easy.
+  rw abs_lt at *,
+  split; -- semicolon means "do next tactic to all goals produced by this tactic"
+  linarith,
 end
 
 /-- If `a(n)` tends to t and `b(n)` tends to `u` then `a(n) - b(n)`
@@ -57,7 +65,6 @@ theorem tendsto_sub {a b : ℕ → ℝ} {t u : ℝ}
   (ha : tendsto a t) (hb : tendsto b u) :
   tendsto (λ n, a n - b n) (t - u) :=
 begin
-  -- this one follows without too much trouble from earlier results.
-  exact tendsto_add ha (tendsto_neg hb)
+  simpa [sub_eq_add_neg] using tendsto_add ha (tendsto_neg hb),
 end
 
